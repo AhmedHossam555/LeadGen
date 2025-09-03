16f28f1a6516a5156c9fa4a4bf33543458fa1975
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-09-04 01:26:13 +0300

    Add or remove 'black-header' class based on scroll position

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  
+  const windowDom = $(window);
+  const headerDom = $('header');
+    // Add or remove 'black-header' class based on scroll position
+  windowDom.scroll(function(){
+    if(windowDom.scrollTop() > 42){
+      headerDom.addClass('black-header');
+    }else{
+      headerDom.removeClass('black-header');
+    }
+  })
 })
